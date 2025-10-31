60e4677114ce7198bddd8d7cb237d7e406161432
Curro Portillo <211curroportillo@gmail.com>
2025-10-31 18:26:51 +0100

    ejercicio 27

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = Integer.parseInt(System.console().readLine("Por favor, introduzca un número entero: "));
+        int factorial = 1;
+        for(int i = 1; i<=numero; i++){
+            factorial *= i;
+        }
+        System.out.printf("%d! = %d", numero, factorial);
     }
 }
